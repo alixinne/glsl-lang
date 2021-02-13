05016c2247fb9d35d5a316680d6986467f9db0f8
Vincent Tavernier <vince.tavernier@gmail.com>
2021-02-13 14:46:12 +0100

    Add basic AST display CLI

---
+++ lang-cli/src/main.rs
@@ -0,0 +1,18 @@
+use std::io::prelude::*;
+
+use glsl_lang::ast::NodeDisplay;
+use glsl_lang::parse::Parse;
+
+fn main() -> anyhow::Result<()> {
+    let mut s = String::new();
+    std::io::stdin().read_to_string(&mut s)?;
+    match glsl_lang::ast::TranslationUnit::parse(s.as_str()) {
+        Ok(tu) => {
+            println!("{}", tu.display());
+        }
+        Err(error) => {
+            eprintln!("error: {:?}", error);
+        }
+    }
+    Ok(())
+}

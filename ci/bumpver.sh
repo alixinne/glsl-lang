2d7b7741e6b84b4f128e0fe1b3bae77efdcbbb6f
Vincent Tavernier <vince.tavernier@gmail.com>
2021-04-05 17:40:19 +0200

    Fix ci/bumpver.sh not updating package.version

--- ci/bumpver.sh
+++ ci/bumpver.sh
@@ -4,5 +4,5 @@ set -eux
 
 . ci/vars
 
-perl -pi -e "BEGIN { our \$a = 0; } if (!\$a) { s/^version = .*/version = \"$1\"/; \$a = 1 }" */Cargo.toml
+perl -pi -e "s/^version = .*/version = \"$1\"/" */Cargo.toml
 cargo upgrade --workspace "glsl-lang@$1" "glsl-lang-quote@$1" "lang-util@$1" "lang-util-derive@$1"

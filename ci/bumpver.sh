fc9f412c53286b5ca43c2b912e299211150bddab
Vincent Tavernier <vince.tavernier@gmail.com>
2022-06-13 00:11:11 +0200

    build: fix ci/bumpver.sh

--- ci/bumpver.sh
+++ ci/bumpver.sh
@@ -5,4 +5,4 @@ set -eux
 . ci/vars
 
 cargo set-version --workspace "$1"
-cargo upgrade --workspace "glsl-lang@$1" "glsl-lang-quote@$1" "lang-util@$1" "lang-util-derive@$1"
+cargo upgrade --workspace "glsl-lang-types@$1" "glsl-lang-pp@$1" "glsl-lang-lexer@$1" "glsl-lang@$1" "glsl-lang-quote@$1" "lang-util@$1" "lang-util-derive@$1" "lang-util-dev@$1"

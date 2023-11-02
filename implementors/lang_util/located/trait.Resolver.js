b838793b2240f1700f49a645c05705ff488a8202
vtavernier <vtavernier@users.noreply.github.com>
2023-11-02 20:46:21 +0000

    deploy: 254a0428269eaed90e6f2f615687eaddaad00ed6

--- implementors/lang_util/located/trait.Resolver.js
+++ implementors/lang_util/located/trait.Resolver.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"glsl_lang_pp":[["impl Resolver for <a class=\"struct\" href=\"glsl_lang_pp/util/line_map/struct.LineMap.html\" title=\"struct glsl_lang_pp::util::line_map::LineMap\">LineMap</a>"],["impl Resolver for <a class=\"struct\" href=\"glsl_lang_pp/processor/expand/struct.ExpandLocation.html\" title=\"struct glsl_lang_pp::processor::expand::ExpandLocation\">ExpandLocation</a>"]],
+"glsl_lang_pp":[["impl Resolver for <a class=\"struct\" href=\"glsl_lang_pp/processor/expand/struct.ExpandLocation.html\" title=\"struct glsl_lang_pp::processor::expand::ExpandLocation\">ExpandLocation</a>"],["impl Resolver for <a class=\"struct\" href=\"glsl_lang_pp/util/line_map/struct.LineMap.html\" title=\"struct glsl_lang_pp::util::line_map::LineMap\">LineMap</a>"]],
 "lang_util":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
